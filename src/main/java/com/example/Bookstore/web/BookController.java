6d4811f7f1372c994aa0d6c5699aa00a39b32a7b
Jani Maaranen <jani.maaranen@saunalahti.fi>
2019-02-06 10:05:25 +0200

    First commit

---
+++ src/main/java/com/example/Bookstore/web/BookController.java
@@ -0,0 +1,21 @@
+package com.example.Bookstore.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.example.Bookstore.domain.Book;
+
+@Controller
+public class BookController {
+
+	@RequestMapping(value="/index", method=RequestMethod.GET)
+	public String GetBook(Model model) {
+		Book book = new Book();
+	
+		model.addAttribute("book", book);
+		return "index";
+	} 
+	
+}
