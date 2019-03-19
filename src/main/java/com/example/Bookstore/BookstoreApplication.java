136d8fdfd8d5518cd2dca49b1f7fb789b67ec10d
Marski96 <jani.maaranen@saunalahti.fi>
2019-03-19 16:19:11 +0200

    first deployment testing

--- src/main/java/com/example/Bookstore/BookstoreApplication.java
+++ src/main/java/com/example/Bookstore/BookstoreApplication.java
@@ -6,6 +6,8 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 
 import com.example.Bookstore.domain.Book;
@@ -16,10 +18,15 @@ import com.example.Bookstore.domain.User;
 import com.example.Bookstore.domain.UserRepository;
 
 @SpringBootApplication
-public class BookstoreApplication {
+public class BookstoreApplication extends SpringBootServletInitializer {
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(BookstoreApplication.class);
+	}
+	
 	private static final Logger log = LoggerFactory.getLogger(BookstoreApplication.class);
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		SpringApplication.run(BookstoreApplication.class, args);
 	}
 	
